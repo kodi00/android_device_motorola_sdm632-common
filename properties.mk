9aa469e381a51f95dbad4668b90158242908e1aa
Rondeau79 <rondeau7@gmail.com>
2020-04-22 15:09:04 -0600

    Update properties.mk

--- properties.mk
+++ properties.mk
@@ -74,6 +74,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0
+    
+# Face Unlock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.face.moto_unlock_service.cam_id=5
 
 # Factory reset partition
 PRODUCT_PROPERTY_OVERRIDES += \
