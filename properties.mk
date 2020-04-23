99afdc082620894209c9784bf0e709d89aa05f0a
Rondeau79 <rondeau7@gmail.com>
2020-04-23 14:07:05 -0600

    Update properties.mk

--- properties.mk
+++ properties.mk
@@ -96,7 +96,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
-
+    sys.use.fifo_ui=1
+    
 # Play store
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase.am=android-motorola \
@@ -207,6 +208,16 @@ persist.rcs.supported=0	persist.rcs.supported=1
 # RCS
 persist.rcs.supported=0	persist.rcs.supported=1
 
+ # LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
 
-    
 
